cc710f70342ddfa42276ea5ada24dbef5b0c5de6
丘海涌 <10620166+qiu-haiyong@user.noreply.gitee.com>
2023-05-26 18:55:02 +0800

    init profect

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// ant design
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+
+Vue.use(Antd);
 Vue.config.productionTip = false;
 
 new Vue({
